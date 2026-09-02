c411c011d0ac7b839683041fd97e038a11aab874
Vidushi Pandey <vidushipandey077@gmail.com>
2026-09-03 02:42:19 +0530

    basic frontend and backend setup done

---
+++ backend/server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+const PORT = 5000;
+
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.send('Backend is running!');
+});
+
+app.listen(PORT, () => {
+  console.log(`Server chal raha hai: http://localhost:${PORT}`);
+});
